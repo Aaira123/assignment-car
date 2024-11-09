9f4330a718cbb3ab570f9f68f7e2fe4e3bd2dd16
sana arshad <160604463+Aaira123@users.noreply.github.com>
2024-11-09 12:45:40 -0800

    first commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+export default function footer(){
+    return(
+        <nav className="bg-red-700 h-28  w-full">
+        <div className="text-white text-center font-bold py-10">
+           &copy; PakWheels 2024 Website made by @Sana Arshad
+        </div>
+        </nav>
+    )
+}
